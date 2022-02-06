5a098d495ffeeb0647bbdc031643cb2843b97be4
Florentin Bürvenich <Florentin@Familie-Buervenich.de>
2022-02-07 00:34:34 +0100

    Add first draft for legal information

---
+++ src/pages/Privacy.vue
@@ -0,0 +1,37 @@
+<template>
+  <Layout>
+    <div class="container-inner mx-auto py-16">
+      <h2 class="text-4xl font-bold mb-16">Privacy policy</h2>
+      <div class="flex flex-row">
+        <div class="md:w-3/5 sm:w-full">
+          <div
+            v-for="(section, idx) in imprintSprivacySectionsections"
+            :key="idx"
+          >
+            <p class="text-2xl font-bold mt-5 mb-2">{{ section.heading }}</p>
+            <p v-for="(paragraph, pIdx) in section.paragraphs" :key="pIdx">
+              {{ paragraph }}
+            </p>
+          </div>
+        </div>
+        <div class="md:w-2/5 sm:w-0">
+          <g-image src="../../static/gdpr.svg" alt="404 page not found" />
+        </div>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Privacy policy',
+  },
+  data: function () {
+    return {
+      privacySections: [],
+    };
+  },
+};
+</script>
+<style scoped></style>
